b353d9bdc8c6de58ecee182db9da8dcbb11e8098
Sucahyana <sucahyana15@gmail.com>
2023-10-13 17:41:55 +0700

    Update ContactForm.jsx Final responsive

--- client/funding-app-v2/src/components/dashboard/TabsComponent/ContactForm.jsx
+++ client/funding-app-v2/src/components/dashboard/TabsComponent/ContactForm.jsx
@@ -1,19 +1,19 @@
-import React, {useState, useEffect} from "react";
-import {ProgressSpinner} from "primereact/progressspinner";
-import {toast} from "react-hot-toast";
-import {ConfirmDialog} from "primereact/confirmdialog";
-import {updateContactProfile} from "../../../services/apiService.jsx"; // Import your API function
-import {RiAlarmWarningFill} from "react-icons/ri";
-import {GiConfirmed} from "react-icons/gi";
-import {useDispatch, useSelector} from "react-redux";
-import {setUser} from "../../../store/actions/user.js";
-import {InputText} from "primereact/inputtext";
-import {Button} from "primereact/button";
+import React, { useState, useEffect } from "react";
+import { ProgressSpinner } from "primereact/progressspinner";
+import { toast } from "react-hot-toast";
+import { ConfirmDialog } from "primereact/confirmdialog";
+import { updateContactProfile } from "../../../services/apiService.jsx";
+import { RiAlarmWarningFill } from "react-icons/ri";
+import { GiConfirmed } from "react-icons/gi";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "../../../store/actions/user.js";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
 
-const ContactForm = ({}) => {
+const ContactForm = () => {
     const dispatch = useDispatch();
-    const userEvents = useSelector(state => state.user?.data?.UserEvents);
-    const userData = useSelector(state => state.user?.data?.user);
+    const userEvents = useSelector((state) => state.user?.data?.UserEvents);
+    const userData = useSelector((state) => state.user?.data?.user);
 
     const [loading, setLoading] = useState(false);
     const [formData, setFormData] = useState({
@@ -22,8 +22,6 @@ const ContactForm = ({}) => {
         other: {
             email: "",
             telpon: "",
-
-
             kataKunci: "",
             isi: "",
         },
@@ -61,54 +59,25 @@ const ContactForm = ({}) => {
         }
     }, [userData]);
 
-    // const validateForm = () => {
-    //     if (!formData.other.email.trim()) {
-    //         toast.error("Email tidak boleh kosong");
-    //         return false;
-    //     }
-    //     // Add more validation checks as needed
-    //     return true;
-    // };
-
     const handleFormSubmit = async () => {
-        // if (!validateForm()) return;
-
         const toastId = toast.loading("Sedang memperbarui informasi kontak...");
 
         try {
-            const requestData = {};
-
-            // Construct the request data with non-empty values
-            if (formData.instagram.trim() !== "") {
-                requestData.instagram = formData.instagram;
-            }
-            if (formData.facebook.trim() !== "") {
-                requestData.facebook = formData.facebook;
-            }
-            if (formData.other && formData.other.email.trim() !== "") { // Add a check for formData.other
-                requestData.email = formData.other.email;
-            }
-            if (formData.other && formData.other.telpon.trim() !== "") { // Add a check for formData.other
-                requestData.telpon = formData.other.telpon;
-            }
-
-
-
-
-
-            // Add logic to handle other values
-            for (const key in formData.other) {
-                const value = formData.other[key].trim();
-                if (value !== "") {
-                    requestData[key] = value;
-                }
-            }
+            const requestData = {
+                ...(formData.instagram.trim() !== "" && { instagram: formData.instagram }),
+                ...(formData.facebook.trim() !== "" && { facebook: formData.facebook }),
+                ...(formData.other.email.trim() !== "" && { email: formData.other.email }),
+                ...(formData.other.telpon.trim() !== "" && { telpon: formData.other.telpon }),
+                ...Object.fromEntries(
+                    Object.entries(formData.other).filter(([key, value]) => value.trim() !== "")
+                ),
+            };
 
             await updateContactProfile(requestData);
             toast.dismiss(toastId);
             toast.success("Informasi kontak berhasil diperbarui");
 
-            // Update the user data in the Redux store
+            // Uncomment the following lines if you want to update the user data in the Redux store
             // const updatedUserData = {
             //     ...userData,
             //     instagram: requestData.instagram || userData.instagram,
@@ -117,7 +86,6 @@ const ContactForm = ({}) => {
             //         ...userData.other,
             //         email: requestData.email || userData.other.email,
             //         telpon: requestData.telpon || userData.other.telpon,
-            //
             //     },
             // };
             // dispatch(setUser(updatedUserData));
@@ -148,17 +116,6 @@ const ContactForm = ({}) => {
             }));
         }
     };
-    const handleAddKeyword = () => {
-        setFormData((prevData) => ({
-            ...prevData,
-            other: {
-                ...prevData.other,
-                [formData.other.kataKunci]: formData.other.isi,
-            },
-        }));
-    };
-
-
 
     const showCancelConfirm = () => {
         setShowCancelConfirmDialog(true);
@@ -176,6 +133,21 @@ const ContactForm = ({}) => {
         setShowSaveConfirmDialog(false);
     };
 
+    const onConfirmCancel = () => {
+        if (userData) {
+            setFormData({
+                ...formData,
+                instagram: userData.instagram || "",
+                facebook: userData.facebook || "",
+                other: {
+                    ...formData.other,
+                    email: userData.email || "",
+                    telpon: userData.telpon || "",
+                },
+            });
+        }
+        hideCancelConfirm();
+    };
     const customStyle = {
         headerTitle: {
             className: 'text-blue-800 font-poppins  text-lg font-medium'
@@ -193,34 +165,17 @@ const ContactForm = ({}) => {
             className: 'flex flex-col items-center'
         }
     };
-
-
-    const onConfirmCancel = () => {
-        if (userData) {
-            setFormData({
-                ...formData,
-                instagram: userData.instagram || "",
-                facebook: userData.facebook || "",
-                other: {
-                    ...formData.other,
-                    email: userData.email || "",
-                    telpon: userData.telpon || "",
-                },
-            });
-        }
-        hideCancelConfirm();
-    };
-
     const onConfirmSave = async () => {
         hideSaveConfirm();
         await handleFormSubmit();
     };
+
     return (
         <>
-            <form className="flex flex-col gap-6 text-gray-800 w-[665px]">
+            <form className="flex flex-col gap-2 md:gap-6 text-gray-800 w-full md:w-[665px]">
                 {/* Email and Telpon fields */}
                 <div className="flex flex-row flex-wrap justify-between">
-                    <div className="flex flex-col gap-2 w-[300px]">
+                    <div className="flex flex-col gap-2 w-full md:w-[calc(50%-10px)]">
                         <label className="text-base font-medium text-blue-950">Email</label>
                         <InputText
                             name="other.email"
@@ -230,7 +185,7 @@ const ContactForm = ({}) => {
                             className="border-2 border-gray-300 rounded p-2 bg-gray-50"
                         />
                     </div>
-                    <div className="flex flex-col gap-2 w-[300px]">
+                    <div className="flex flex-col gap-2 w-full md:w-[calc(50%-10px)]">
                         <label className="text-base font-medium text-blue-950">Telpon</label>
                         <InputText
                             name="other.telpon"
@@ -243,9 +198,9 @@ const ContactForm = ({}) => {
                 </div>
 
                 {/* Instagram and Facebook fields */}
-                <hr className="h-0.5"/>
-                <div className="flex flex-row flex-wrap gap-6 justify-between">
-                    <div className="flex flex-col gap-2 w-[300px]">
+                <hr className="h-0.5" />
+                <div className="flex flex-row flex-wrap justify-between">
+                    <div className="flex flex-col gap-2 w-full md:w-[calc(50%-10px)]">
                         <label className="text-base font-medium text-blue-950">Instagram</label>
                         <InputText
                             name="instagram"
@@ -255,8 +210,8 @@ const ContactForm = ({}) => {
                             className="border-2 border-gray-300 rounded p-2 bg-gray-50"
                         />
                     </div>
-                    <div className="flex flex-col gap-2 w-[300px]">
-                        <label className="text-base font-medium text-blue-950">Facebook</label>
+                    <div className="flex flex-col gap-2 w-full md:w-[calc(50%-10px)]">
+                        <label className="text-base font-medium text-blue-950 ">Facebook</label>
                         <InputText
                             name="facebook"
                             type="text"
@@ -265,52 +220,51 @@ const ContactForm = ({}) => {
                             className="border-2 border-gray-300 rounded p-2 bg-gray-50"
                         />
                     </div>
+
                 </div>
 
-
-
-
                 {/* Kata Kunci and Isi fields */}
-                <hr className="h-0.5"/>
-                <div className="flex flex-row flex-wrap gap-6 justify-between">
-                    <div className="flex flex-col gap-2 w-[300px]">
+                <hr className="h-0.5" />
+                <div className="flex flex-row flex-wrap justify-between">
+                    <div className="flex flex-col gap-2 w-full md:w-[calc(50%-10px)]">
                         <label className="text-base font-medium text-blue-950">Kata Kunci</label>
                         <InputText
                             name="kataKunci"
                             type="text"
                             value={formData.kataKunci}
                             onChange={handleInputChange}
+                            disabled={true}
                             className="border-2 border-gray-300 rounded p-2 bg-gray-50"
                         />
                     </div>
-                    <div className="flex flex-col gap-2 w-[300px]">
+                    <div className="flex flex-col gap-2 w-full md:w-[calc(50%-10px)]">
                         <label className="text-base font-medium text-blue-950">Isi</label>
                         <InputText
                             name="isi"
                             type="text"
                             value={formData.isi}
                             onChange={handleInputChange}
+                            disabled={true}
                             className="border-2 border-gray-300 rounded p-2 bg-gray-50"
                         />
                     </div>
                 </div>
 
-
                 <div className="flex flex-row gap-6 justify-end text-gray-100 mt-8">
                     <Button
                         type="button"
-                        className="w-[200px] rounded-xl bg-blue-300 shadow-md py-3 px-6 flex flex-row items-center justify-center gap-2"
+                        className=" rounded-xl bg-blue-300 shadow-md py-3 px-6 flex flex-row items-center justify-center gap-2"
                         onClick={showCancelConfirm}
                     >
                         Batal
                     </Button>
                     <Button
                         type="button"
-                        className="w-[200px]  rounded-xl bg-blue-600 shadow-md py-3 px-6 flex flex-row items-center justify-center gap-2"
+                        className=" rounded-xl bg-blue-600 shadow-md py-3 px-6 flex flex-row items-center justify-center gap-2"
                         onClick={showSaveConfirm}
                     >
                         {loading ? (
-                            <ProgressSpinner style={{width: "24px", height: "24px"}} strokeWidth="4"/>
+                            <ProgressSpinner style={{ width: "24px", height: "24px" }} strokeWidth="4" />
                         ) : (
                             "Simpan"
                         )}
