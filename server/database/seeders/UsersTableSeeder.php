601f7ffa51e10918025355c5acb847b85504496c
sucahyana <sucahyana15@gmail.com>
2025-12-23 20:12:36 +0700

    setup: make Laravel backend runnable

--- server/database/seeders/UsersTableSeeder.php
+++ server/database/seeders/UsersTableSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
